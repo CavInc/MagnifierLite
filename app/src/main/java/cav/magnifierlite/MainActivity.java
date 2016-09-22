e62968f4ea94784ecdea44fb1b8484c9ae52a295
CavInc <cavinc20@gmail.com>
2016-09-22 12:45:57 +0700

    Настройка формы

--- app/src/main/java/cav/magnifierlite/MainActivity.java
+++ app/src/main/java/cav/magnifierlite/MainActivity.java
@@ -1,13 +1,30 @@
 package cav.magnifierlite;
 
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = "MAGNIFER";
+
+    private SurfaceView sv;
+    private SurfaceHolder holder;
+    private Camera camera;
+    private TextView zoomText;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        zoomText = (TextView) findViewById(R.id.zoom_text);
+
+        sv = (SurfaceView) findViewById(R.id.surfaceView);
+
     }
 }
