c2ec3564d302a982925eb8924735935b8cdd2e59
CavInc <cavinc20@gmail.com>
2016-09-23 15:50:56 +0700

    Добавлены иконки приложения рабочии иконки сделаны белым добавлен вариант установки параметров вьюпорта и камеры. добавлен метод для установки фокуса но еще не реализован

--- app/src/main/java/cav/magnifierlite/MainActivity.java
+++ app/src/main/java/cav/magnifierlite/MainActivity.java
@@ -1,15 +1,21 @@
 package cav.magnifierlite;
 
 import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.graphics.Matrix;
+import android.graphics.RectF;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
@@ -39,6 +45,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private boolean isZoom = false;
     private boolean isFlashMode = false;
     private int maxZoom;
+
+    private List<String> colorEffect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,6 +85,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             camera = Camera.open(CAMERA_ID);
         }else {
             Toast.makeText(this,"Старый ведройд",Toast.LENGTH_LONG).show();
+            camera = Camera.open();
         }
         checkPreferns();
         setStartFocus();
@@ -135,6 +144,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 params.setFlashMode(Parameters.FLASH_MODE_TORCH);
             }
             camera.setParameters(params);
+        }else {
+            showToast(getResources().getString(R.string.no_flash));
         }
 
     }
@@ -167,6 +178,127 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    // Установка фокуса по касанию экрана
+    private void setFocusManual(){
+        Parameters params = camera.getParameters();
+        if (params.getMaxNumFocusAreas() > 0){
+
+        }
+    }
+
+    private void showToast(String message){
+        Toast.makeText(this,message,Toast.LENGTH_LONG).show();
+    }
+
+
+    private void setResizeViewPort(){
+        Camera.Size previewSize = camera.getParameters().getPreviewSize();
+        float aspect = (float) previewSize.width / previewSize.height;
+
+        int previewSurfaceWidth = sv.getWidth();
+        int previewSurfaceHeight = sv.getHeight();
+
+        LayoutParams lp = sv.getLayoutParams();
+
+        // здесь корректируем размер отображаемого preview, чтобы не было искажений
+        if (this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE){
+            // портретный вид
+            camera.setDisplayOrientation(90);
+            lp.height = previewSurfaceHeight;
+            lp.width = (int) (previewSurfaceHeight / aspect);
+        }else {
+            // ландшафтный
+            camera.setDisplayOrientation(0);
+            lp.width = previewSurfaceWidth;
+            lp.height = (int) (previewSurfaceWidth / aspect);
+        }
+        sv.setLayoutParams(lp);
+        camera.startPreview();
+    }
+
+
+    // поворот экрана
+    private void setCameraDisplayOrientation(int cameraId) {
+        // определяем насколько повернут экран от нормального положения
+        int rotation = getWindowManager().getDefaultDisplay().getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                degrees = 0;
+                break;
+            case Surface.ROTATION_90:
+                degrees = 90;
+                break;
+            case Surface.ROTATION_180:
+                degrees = 180;
+                break;
+            case Surface.ROTATION_270:
+                degrees = 270;
+                break;
+        }
+
+        int result = 0;
+
+        // получаем инфо по камере cameraId
+        Camera.CameraInfo info = new Camera.CameraInfo();
+        Camera.getCameraInfo(cameraId, info);
+
+        // задняя камера
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
+            result = ((360 - degrees) + info.orientation);
+        } else
+            // передняя камера
+            if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+                result = ((360 - degrees) - info.orientation);
+                result += 360;
+            }
+        result = result % 360;
+        camera.setDisplayOrientation(result);
+    }
+
+    private void setPreviewSize(boolean fullScreen) {
+        // получаем размеры экрана
+        Display display = getWindowManager().getDefaultDisplay();
+        boolean widthIsMax = display.getWidth() > display.getHeight();
+
+        // определяем размеры превью камеры
+        Camera.Size size = camera.getParameters().getPreviewSize();
+
+        RectF rectDisplay = new RectF();
+        RectF rectPreview = new RectF();
+
+        // RectF экрана, соотвествует размерам экрана
+        rectDisplay.set(0, 0, display.getWidth(), display.getHeight());
+
+        // RectF первью
+        if (widthIsMax) {
+            // превью в горизонтальной ориентации
+            rectPreview.set(0, 0, size.width, size.height);
+        } else {
+            // превью в вертикальной ориентации
+            rectPreview.set(0, 0, size.height, size.width);
+        }
+
+        Matrix matrix = new Matrix();
+        // подготовка матрицы преобразования
+        if (!fullScreen) {
+            // если превью будет "втиснут" в экран (второй вариант из урока)
+            matrix.setRectToRect(rectPreview, rectDisplay,
+                    Matrix.ScaleToFit.START);
+        } else {
+            // если экран будет "втиснут" в превью (третий вариант из урока)
+            matrix.setRectToRect(rectDisplay, rectPreview,
+                    Matrix.ScaleToFit.START);
+            matrix.invert(matrix);
+        }
+        // преобразование
+        matrix.mapRect(rectPreview);
+
+        // установка размеров surface из получившегося преобразования
+        sv.getLayoutParams().height = (int) (rectPreview.bottom);
+        sv.getLayoutParams().width = (int) (rectPreview.right);
+    }
+
     class HolderCallback implements SurfaceHolder.Callback {
 
         @Override
@@ -176,18 +308,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 camera.startPreview(); // начало трансляции
             } catch (IOException e) {
                 e.printStackTrace();
+                showToast(e.getLocalizedMessage());
             }
+            setResizeViewPort();
         }
 
         @Override
         public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int width, int height) {
+            /*
             camera.stopPreview();// остановили трансляцию
+            setCameraDisplayOrientation(CAMERA_ID);
             try {
                 camera.setPreviewDisplay(holder);
                 camera.startPreview();
             } catch (Exception e) {
                 e.printStackTrace();
+                showToast(e.getLocalizedMessage());
             }
+            */
         }
 
         @Override
